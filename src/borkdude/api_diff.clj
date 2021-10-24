035fdec59e21f19e2b4b206ed20ed32c2381afb5
lread <lee@dlread.com>
2021-10-24 18:57:36 -0400

    Draft crack at separating reporting from analysis
    Add new option `:format`
    - when `:edn` will print output as clojure map
    - otherwise continues to print output as report
    
    Will follow up with updated docs in subsequent commit.

--- src/borkdude/api_diff.clj
+++ src/borkdude/api_diff.clj
@@ -21,7 +21,9 @@
        (sort-by (juxt :ns :row :col))
        (map (fn [v]
               [((juxt :ns :name) v)
-               (select-keys v [:ns :name :fixed-arities :varargs-min-arity :deprecated :private ::excluded])]))))
+               (select-keys v [:ns :name :row :col :filename
+                               :fixed-arities :varargs-min-arity
+                               :deprecated :private ::excluded-meta])]))))
 
 (defn vars [lib exclude-meta]
   (->> (if exclude-meta
@@ -42,22 +44,41 @@
                 (map #(if-let [excluded-by (some-> (merge (:meta %) (get ns-meta-excludes (:ns %)))
                                                    (select-keys exclude-meta)
                                                    seq)]
-                        (assoc % ::excluded (format "meta %s" (vec (keys excluded-by))))
+                        (assoc % ::excluded-meta (-> excluded-by keys sort vec))
                         %))))
          (->> (clj-kondo/run! {:lint   [lib]
                                :config {:output {:analysis true :format :edn}}})
               :analysis :var-definitions))
        (remove #(some #{(:defined-by %)} ['cljs.core/declare 'clojure.core/declare]))))
 
-(defn log-diff [level v  msg]
-  (let [{:keys [filename row col ns name]} v]
-    (println (str filename ":" row ":" col ": " level ": " ns "/" name " " msg))))
+(defn reg-finding! [findings m v1 v2]
+  (swap! findings conj (cond-> (assoc m
+                                      :v1 (select-keys v1 [:row :col :filename])
+                                      :ns (:ns v1)
+                                      :name (:name v1))
+                         v2 (assoc :v2 (select-keys v2 [:row :col :filename])))))
 
 (defn- force-os-path-syntax
   "see https://github.com/clj-kondo/clj-kondo/issues/1438
   can turf if/when this issue is fixed"
   [path]
   (some-> path io/file str))
+
+(defn- report-text [result]
+  (doseq [f (:findings result)
+          :let [v1 (:v1 f)]]
+    (println (str (:filename v1) ":"
+                  (:row v1) ":"
+                  (:col v1) ": "
+                  (name (:level f)) ": "
+                  (:ns f) "/" (:name f) " "
+                  (case (:type f)
+                    :var "was removed"
+                    :now-private "has become private"
+                    :deprecated "was deprecated"
+                    :arity (format "arity %s was removed" (:data f))
+                    :meta (format "now has meta %s" (:data f)))
+                  "."))))
 
 (defn api-diff [{:keys [lib v1 v2
                         path1 path2
@@ -68,25 +89,26 @@
         vars-2 (vars path2 exclude-meta)
         compare-group-1 (group vars-1)
         compare-group-2-lookup (into {} (group vars-2))
-        lookup-1 (index-by (juxt :ns :name) vars-1)]
+        findings (atom [])]
     (doseq [[k var-1] compare-group-1]
-      (when (and (not (:private var-1)) (not (::excluded var-1)))
+      (when (and (not (:private var-1)) (not (::excluded-meta var-1)))
         (if-let [var-2 (get compare-group-2-lookup k)]
           (do
             (when (:private var-2)
-              (log-diff "error" (get lookup-1 k) "has become private."))
+              (reg-finding! findings {:level :error :diff :removal :type :now-private} var-1 var-2))
             (let [fixed-arities-v1  (:fixed-arities var-1)
                   fixed-arities-v2  (:fixed-arities var-2)
                   varargs-min-arity (:varargs-min-arity var-2)]
               (doseq [arity fixed-arities-v1]
                 (when-not (or (contains? fixed-arities-v2 arity)
                               (and varargs-min-arity (>= arity varargs-min-arity)))
-                  (log-diff "error" (get lookup-1 k) (format "arity %s was removed." arity)))))
+                  (reg-finding! findings {:level :error :diff :removal :type :arity :data arity} var-1 var-2))))
             (when (and (:deprecated var-2) (not (:deprecated var-1)))
-              (log-diff "warning" (get lookup-1 k) "was deprecated."))
-            (when (::excluded var-2)
-              (log-diff "warning" (get lookup-1 k) (format "now has %s." (::excluded var-2)))))
-          (log-diff "error" (get lookup-1 k) "was removed."))))))
+              (reg-finding! findings {:level :warning :diff :removal :type :deprecated} var-1 var-2))
+            (when (::excluded-meta var-2)
+              (reg-finding! findings {:level :warning :diff :removal :type :meta :data (::excluded-meta var-2)} var-1 var-2)))
+          (reg-finding! findings {:level :error :diff :removal :type :var} var-1 nil))))
+    @findings))
 
 (defn- to-keyword [s]
   (keyword
@@ -111,7 +133,13 @@
   (let [{:keys [lib v1 v2 path1 path2] :as opts}
         (parse-opts args {:exclude-meta {:parse-fn to-keyword
                                          :collect-fn (fnil conj #{})}
-                          :lib {:parse-fn symbol}})]
+                          :lib {:parse-fn symbol}
+                          :format {:parse-fn to-keyword}})]
     (when-not (or (and lib v1 v2) (and path1 path2))
       (throw (ex-info "must specify either :lib, :v1 and :v2 OR :path1 and :path2" {})))
-    (api-diff opts)))
+    (let [result {:args (vec args)
+                  :opts opts
+                  :findings (api-diff opts)}]
+      (if (= :edn (:format opts))
+        (prn result)
+        (report-text result)))))
--- test/borkdude/api_diff_test.clj
+++ test/borkdude/api_diff_test.clj
@@ -1,18 +1,26 @@
 (ns borkdude.api-diff-test
   (:require [borkdude.api-diff :as api-diff]
+            [clojure.edn :as edn]
             [clojure.string :as str]
-            [clojure.test :as t :refer [deftest testing is]]))
+            [clojure.test :as t :refer [deftest testing is]]
+            [clojure.walk :as walk]))
+
+(defn- osify-path [p]
+  (str/replace p "/" (System/getProperty "file.separator")))
 
 (defn- osify [lines]
   (mapv (fn [l]
           (if (str/starts-with? l "test-resources")
             (let [[p r] (str/split l #" " 2)]
-              (str
-               (str/replace p "/" (System/getProperty "file.separator"))
-               " "
-               r))
+              (str (osify-path p) " " r))
             l))
         lines))
+
+(defn- osify-findings [findings]
+  (walk/postwalk #(if-let [f (:filename %)]
+                    (assoc % :filename (osify-path f))
+                    %)
+                 findings))
 
 (deftest diff-test
   (testing "libs"
@@ -64,6 +72,7 @@
                        "test-resources/older/example.clj:18:1: error: example/arity-change-and-becomes-skip-wiki arity 2 was removed."])
                actual-lines))))
     (testing "exclude-meta multiple"
+
       (let [actual-lines (-> (api-diff/-main ":path1" "test-resources/older"
                                              ":path2" "test-resources/newer"
                                              ":exclude-meta" ":no-doc"
@@ -76,5 +85,55 @@
                        "test-resources/older/example.clj:12:1: warning: example/z was deprecated."
                        "test-resources/older/example.clj:18:1: error: example/arity-change-and-becomes-skip-wiki arity 2 was removed."
                        "test-resources/older/example.clj:18:1: warning: example/arity-change-and-becomes-skip-wiki now has meta [:skip-wiki]."])
-               actual-lines))))))
-;; => #'borkdude.api-diff-test/diff-test
+               actual-lines)))
+      (testing "structured output"
+        (let [actual-edn (-> (api-diff/-main ":path1" "test-resources/older"
+                                             ":path2" "test-resources/newer"
+                                             ":exclude-meta" ":no-doc"
+                                             ":exclude-meta" ":skip-wiki"
+                                             ":format" ":edn")
+                             with-out-str
+                             edn/read-string)]
+          (is (= {:args [":path1" "test-resources/older" ":path2" "test-resources/newer"
+                         ":exclude-meta" ":no-doc" ":exclude-meta" ":skip-wiki" ":format" ":edn"]
+                  :opts {:cmds [] :path1 "test-resources/older" :path2 "test-resources/newer"
+                         :exclude-meta #{:no-doc :skip-wiki} :format :edn}
+                  :findings (osify-findings '[{:level :error,
+                                               :diff :removal,
+                                               :type :now-private,
+                                               :v1 {:row 6, :col 1, :filename "test-resources/older/example.clj"},
+                                               :ns example, :name becomes-private,
+                                               :v2 {:row 8, :col 1, :filename "test-resources/newer/example.clj"}}
+                                              {:level :warning,
+                                               :diff :removal,
+                                               :type :meta,
+                                               :data [:no-doc],
+                                               :v1 {:row 8, :col 1, :filename "test-resources/older/example.clj"},
+                                               :ns example, :name becomes-nodoc,
+                                               :v2 {:row 11, :col 1, :filename "test-resources/newer/example.clj"}}
+                                              {:level :error,
+                                               :diff :removal,
+                                               :type :var,
+                                               :v1 {:row 11, :col 1, :filename "test-resources/older/example.clj"},
+                                               :ns example, :name y}
+                                              {:level :warning,
+                                               :diff :removal,
+                                               :type :deprecated,
+                                               :v1 {:row 12, :col 1, :filename "test-resources/older/example.clj"},
+                                               :ns example, :name z,
+                                               :v2 {:row 15, :col 1, :filename "test-resources/newer/example.clj"}}
+                                              {:level :error,
+                                               :diff :removal,
+                                               :type :arity,
+                                               :data 2,
+                                               :v1 {:row 18, :col 1, :filename "test-resources/older/example.clj"},
+                                               :ns example, :name arity-change-and-becomes-skip-wiki,
+                                               :v2 {:row 21, :col 1, :filename "test-resources/newer/example.clj"}}
+                                              {:level :warning,
+                                               :diff :removal,
+                                               :type :meta,
+                                               :data [:skip-wiki],
+                                               :v1 {:row 18, :col 1, :filename "test-resources/older/example.clj"},
+                                               :ns example, :name arity-change-and-becomes-skip-wiki,
+                                               :v2 {:row 21, :col 1, :filename "test-resources/newer/example.clj"}}])}
+                 actual-edn)))))))
